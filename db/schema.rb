20cdac1c54cbe96dd4d815d16d56a542ffe11762
aucchwas <ucchwasa@gmail.com>
2024-11-18 17:27:34 -0600

    done most but tired and gon do something else rn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_14_232816) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_16_004445) do
   create_table "book_genres", force: :cascade do |t|
     t.integer "book_id", null: false
     t.integer "genre_id", null: false
@@ -33,6 +33,18 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_14_232816) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "book_genres", "books"
